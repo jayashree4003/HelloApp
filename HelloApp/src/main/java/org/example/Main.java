17088afb7d75fa924fe49fa5901b6e6cad3dca48
jayashree4003 <jayashree4003@gmail.com>
2026-03-30 14:49:18 +0530

    Coded UC4: Display Hello with Multiple Command-Line Arguments

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,8 +1,21 @@
-package org.example;
+packag
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-        public static void main(String[] args) {
-        }
-    }
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        String name = "World";
+
+        if (args.length > 0) {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
